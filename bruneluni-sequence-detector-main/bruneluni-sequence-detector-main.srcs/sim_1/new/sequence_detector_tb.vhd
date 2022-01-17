9ef6c2048420bd178f369f1910bb3dcf86d80881
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-17 02:39:57 +0000

    modified test bench file with correct test sequence

--- bruneluni-sequence-detector-main/bruneluni-sequence-detector-main.srcs/sim_1/new/sequence_detector_tb.vhd
+++ bruneluni-sequence-detector-main/bruneluni-sequence-detector-main.srcs/sim_1/new/sequence_detector_tb.vhd
@@ -1,12 +1,34 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.Std_logic_1164.all;
+use IEEE.Numeric_Std.all;
+
 entity SequenceDetectorTb is
---  Port ( );
-end SequenceDetectorTb;
+end;
 
 architecture BehaviouralSequenceDetectorTb of SequenceDetectorTb is
 
+  component SequenceDetector port (
+      value: in std_logic_vector(0 to 3);
+      clk: in std_logic
+  );
+  end component;
+
+  signal value: std_logic_vector(0 to 3);
+  signal clk: std_logic ;
+
 begin
 
+  uut: SequenceDetector port map ( value => value,
+                                   clk   => clk );
 
-end BehaviouralSequenceDetectorTb;
+  stimulus: process
+  begin
+    
+    clk <= '0', '1' after 10 ns, '0' after 10 ns, '1' after 10 ns, '0' after 10 ns, '1' after 10 ns, '0' after 10 ns, '1' after 10 ns, '0' after 10 ns, '1' after 10 ns;
+    value <= "0000", "0001" after 15 ns, "1000" after 15 ns, "0010" after 15 ns, "0100" after 15 ns, "0100" after 15 ns;
+
+    wait;
+  end process;
+
+
+end;
