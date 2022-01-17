a00773caf990c4557693ec02ecc8091b1a469b97
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-17 02:19:01 +0000

    added test bench

---
+++ bruneluni-sequence-detector-main/bruneluni-sequence-detector-main.srcs/sim_1/new/sequence_detector_tb.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+entity SequenceDetectorTb is
+--  Port ( );
+end SequenceDetectorTb;
+
+architecture BehaviouralSequenceDetectorTb of SequenceDetectorTb is
+
+begin
+
+
+end BehaviouralSequenceDetectorTb;
