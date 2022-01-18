61780e16fa1e64abee7163465606a470c66e82cb
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-18 15:09:10 +0000

    standardize on snake case

--- src/sequence_detector_tb.vhd
+++ src/sequence_detector_tb.vhd
@@ -1,6 +1,6 @@
-library IEEE;
-use IEEE.Std_logic_1164.all;
-use IEEE.Numeric_Std.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity sequence_detector_tb is
 end;
