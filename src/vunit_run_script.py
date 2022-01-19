8b55719ebd4aa225df79718d0d3219f9cafbbdf6
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-19 00:47:11 +0000

    add src path start to vunit_run_script src finder

--- src/vunit_run_script.py
+++ src/vunit_run_script.py
@@ -8,7 +8,7 @@ vu = VUnit.from_argv()
 lib = vu.add_library("lib")
 
 # Add all files ending in .vhd in current working directory to library
-lib.add_source_files(Path("tb_example.vhd").resolve())
+lib.add_source_files(Path("src/tb_example.vhd").resolve())
 
 # Run vunit function
 vu.main()
