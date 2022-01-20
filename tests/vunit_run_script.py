c7fd0ae0278a4afe3236ea37799e084786eeedfb
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-20 21:03:11 +0000

    made vhd file search for windows and linux

--- tests/vunit_run_script.py
+++ tests/vunit_run_script.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 from vunit import VUnit
+import os
 
 vu = VUnit.from_argv()
 lib = vu.add_library("lib")
-lib.add_source_files(Path("src/*.vhd").resolve())
-lib.add_source_files(Path("tests/*.vhd").resolve())
+lib.add_source_files(f"{os.path.dirname(os.path.realpath(__file__))}/../src/*.vhd")
+lib.add_source_files(f"{os.path.dirname(os.path.realpath(__file__))}/../tests/*.vhd")
 vu.main()
