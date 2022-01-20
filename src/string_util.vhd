bdd4537dcc800f49924e317fea7de4e5abeb7fb3
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-20 12:04:13 +0000

    changed string util - to take the whole vector array, including first st_logic - to not reverse the array

--- src/string_util.vhd
+++ src/string_util.vhd
@@ -5,9 +5,9 @@ package string_util_pkg is
 end;
 package body string_util_pkg is
     function std_logic_vector_to_string(vector: std_logic_vector) return string is
-        variable vector_string : string (vector'length-1 downto 1) := (others => NUL);
+        variable vector_string : string (1 to vector'length) := (others => NUL);
     begin
-        for i in vector'length-1 downto 1 loop
+        for i in 1 to vector'length loop
             vector_string(i) := std_logic'image(vector((i-1)))(2);
         end loop;
     return vector_string;
