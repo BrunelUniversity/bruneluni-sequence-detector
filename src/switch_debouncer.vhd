996a099f3958996ae7cbb6c442ed329f22bcbc53
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-22 17:45:42 +0000

    added logging to debouncer and fixed failing test - added logging of internal state - last buttons and clk count are both incremented clk count check

--- src/switch_debouncer.vhd
+++ src/switch_debouncer.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use work.log_util_pkg.all;
 
 entity switch_debouncer is Port (
     clk: in std_logic;
@@ -15,20 +16,23 @@ begin
         variable last_buttons: integer range 0 to 4:=0 ;
         variable clk_count: integer range 0 to 1:=0;
         variable last_stable_buttons: integer range 0 to 4:=0;
-        variable btn_var: integer range 0 to 4:=0;
         variable btn_stable: bit:='0';
     begin
-    btn_var:=btn;
     if rising_edge(clk) then
+        log("last_buttons: "&integer'image(last_buttons));
+        log(" === current_buttons: "&integer'image(btn));
+        log(" === clk_count: "&integer'image(clk_count));
+        log(" === last_stable_buttons: "&integer'image(last_stable_buttons));
+        log_line(" === btn_stable: "&bit'image(btn_stable));
         if clk_count=1 then
-            if (btn_var = last_buttons and last_stable_buttons/=btn) then
+            if (btn = last_buttons and last_stable_buttons/=btn) then
             btn_stable:='1';
-            last_stable_buttons:=btn_var;
+            last_stable_buttons:=btn;
             clk_count:=0;
-            last_buttons:=btn_var;
             end if;
-        clk_count:=clk_count+1;
         end if;
+        last_buttons:=btn;
+        clk_count:=clk_count+1;
     end if;
     buttons_stable<=btn_stable;
     end process;
