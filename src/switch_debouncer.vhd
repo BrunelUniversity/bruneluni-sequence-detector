9856a2b9d07a7b469291aea1e293410cafad19ad
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-23 13:08:06 +0000

    added test for when button is held down - tests fail

--- src/switch_debouncer.vhd
+++ src/switch_debouncer.vhd
@@ -14,7 +14,7 @@ architecture behavioral_switch_debouncer of switch_debouncer is
 begin
     process(clk)
         variable last_buttons: integer range 0 to 4:=0 ;
-        variable clk_count: integer range 0 to 1:=0;
+        variable clk_count: integer:=0;
         variable last_stable_buttons: integer range 0 to 4:=0;
         variable btn_stable: bit:='0';
     begin
@@ -25,15 +25,19 @@ begin
         log(" === last_stable_buttons: "&integer'image(last_stable_buttons));
         log_line(" === btn_stable: "&bit'image(btn_stable));
         if clk_count=1 then
-            if (btn = last_buttons and last_stable_buttons/=btn) then
-            btn_stable:='1';
-            last_stable_buttons:=btn;
-            clk_count:=0;
+            if (btn = last_buttons and (not(last_stable_buttons=btn))) then
+                clk_count:=0;
+                buttons_stable<='1';
+                last_stable_buttons:=btn;
+            else
+                buttons_stable<='0';
             end if;
         end if;
         last_buttons:=btn;
         clk_count:=clk_count+1;
+        if not(last_stable_buttons=btn) then
+            clk_count:=0;
+        end if;
     end if;
-    buttons_stable<=btn_stable;
     end process;
 end;
