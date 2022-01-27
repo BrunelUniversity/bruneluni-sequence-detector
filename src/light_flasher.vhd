24fd4ca0d3702944670b8048249aca908e49ec5e
Aidan Gannon (Student) <1804670@brunel.ac.uk>
2022-01-27 21:21:41 +0000

    added light flasher component

---
+++ src/light_flasher.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity light_flasher is
+--  Port ( );
+end light_flasher;
+
+architecture behavioral_light_flasher of light_flasher is
+
+begin
+
+
+end behavioral_light_flasher;
