aef6cc9587d7d7f58222946bfdcd862bbf63b95f
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-28 19:32:18 +0000

    added basic byte logging

---
+++ logger_client.py
@@ -0,0 +1,10 @@
+import serial
+import binascii
+
+ser = serial.Serial(port='COM3', baudrate=1000, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
+while(True):
+    end = " "
+    stuff = ser.read(1)
+    if stuff == str.encode('|'):
+        end = "\n"
+    print((bin(int.from_bytes(stuff, 'little'))), end=end)
