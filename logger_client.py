53e0fd5e566895822349200365ecee8f6d111404
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-28 19:33:06 +0000

    formatted python

--- logger_client.py
+++ logger_client.py
@@ -1,7 +1,13 @@
 import serial
-import binascii
 
-ser = serial.Serial(port='COM3', baudrate=1000, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
+ser = serial.Serial(port='COM3',
+                    baudrate=1000,
+                    bytesize=8,
+                    parity='N',
+                    stopbits=1,
+                    timeout=None,
+                    xonxoff=0,
+                    rtscts=0)
 while(True):
     end = " "
     stuff = ser.read(1)
